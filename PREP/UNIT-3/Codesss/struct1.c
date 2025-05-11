129debbf184b1fc9619005415a491bf04d6af04d
sabique <sabiqueislam7@gmail.com>
2025-05-11 22:39:51 +0530

    Struct Snippets

---
+++ PREP/UNIT-3/Codesss/struct1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct Student{
+    int id;
+    char name[100];
+    char favourite_subject[100];
+} student1, student2, student3;
+
+int main(){
+
+    student1.id = 1;  // (.) --> Structure Member Operator
+    strcpy(student1.name, "John");  // strcpy --> String Copy (string.h
+    strcpy(student1.favourite_subject, "Maths");
+
+
+    printf("Student 1: %d, %s, %s\n", student1.id, student1.name, student1.favourite_subject);
+    
+    return 0;
+}
