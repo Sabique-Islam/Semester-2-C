9de703aafc13480901da1d8a5aae54af62610cf7
sabique <sabiqueislam7@gmail.com>
2025-05-21 12:33:57 +0530

    Modular Programming

---
+++ Class/May-21/Headerfile/main.c
@@ -0,0 +1,10 @@
+#include "program-1.h"
+
+int main(){
+    int a = 10, b = 5;
+    printf("Addition: %d\n", add(a, b));
+    printf("Subtraction: %d\n", sub(a, b));
+    printf("Multiplication: %d\n", mul(a, b));
+    printf("Division: %d\n", div(a, b));
+    return 0;
+}
